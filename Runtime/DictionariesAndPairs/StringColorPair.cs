f6d92b826478919eafc0bd2b7483c365c991dc17
Cippman <cipponessuno@gmail.com>
2022-04-25 14:18:05 +0200

    added stringcolor pair and stringstring pair

---
+++ Runtime/DictionariesAndPairs/StringColorPair.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace CippSharp.Core.Containers
+{
+    [Serializable]
+    public class StringColorPair : SerializedKeyValuePair<string, Color>
+    {
+        /// <summary>
+        /// Empty StringColor
+        /// </summary>
+        public static readonly StringColorPair Empty = new StringColorPair("", default(Color));
+
+        protected StringColorPair()
+        {
+            
+        }
+
+        public StringColorPair(string name, Color color)
+        {
+            this.key = name;
+            this.value = color;
+        }
+    }
+}
---
+++ Runtime/DictionariesAndPairs/StringStringPair.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CippSharp.Core.Containers
+{
+    [Serializable]
+    public class StringStringPair : SerializedKeyValuePair<string, string>, IEquatable<StringStringPair>
+    {
+        public static readonly StringStringPair Empty = new StringStringPair();
+        
+        public StringStringPair()
+        {
+            
+        }
+
+        public StringStringPair(string key, string value) : this()
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        #region Equality Memebers
+
+        public bool Equals(StringStringPair other)
+        {
+            return string.Equals(key, other.key) && string.Equals(value, other.value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is StringStringPair other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((key != null ? key.GetHashCode() : 0) * 397) ^ (value != null ? value.GetHashCode() : 0);
+            }
+        }
+
+        public static bool operator ==(StringStringPair left, StringStringPair right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(StringStringPair left, StringStringPair right)
+        {
+            return !left.Equals(right);
+        }
+
+        #endregion
+    }
+}
