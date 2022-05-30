677cc184cfc636b8bba6899cbc78cd9e5b6e4156
Cippman <cipponessuno@gmail.com>
2022-05-30 14:24:29 +0200

    added string object pair and his dictionary

---
+++ Runtime/DictionariesAndPairs/DictionaryStringObjectPairs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+
+namespace CippSharp.Core.Containers
+{
+    [Serializable]
+    public class DictionaryStringObjectPairs : SerializedDictionary<StringObjectPair, string, Object>
+    {
+        public DictionaryStringObjectPairs() : base()
+        {
+            
+        }
+        
+        public DictionaryStringObjectPairs(IEnumerable<StringObjectPair> range) : base(range)
+        {
+            
+        }
+    }
+}
---
+++ Runtime/DictionariesAndPairs/StringObjectPair.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CippSharp.Core.Containers
+{
+    [Serializable]
+    public class StringObjectPair : SerializedKeyValuePair<string, UnityEngine.Object>
+    {
+        /// <summary>
+        /// Empty StringObject
+        /// </summary>
+        public static readonly StringObjectPair Empty = new StringObjectPair(string.Empty, null);
+
+        protected StringObjectPair()
+        {
+            
+        }
+
+        public StringObjectPair(string name, UnityEngine.Object o)
+        {
+            this.key = name;
+            this.value = o;
+        }
+    }
+}
